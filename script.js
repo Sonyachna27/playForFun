3a38c94db75dda197f720e55bfa7c114edb2f6e3
Sonyachn27 <116268317+Sonyachn27@users.noreply.github.com>
2024-02-14 18:29:22 +0200

    change image and add new function for first section

--- script.js
+++ script.js
@@ -1,11 +1,9 @@
 const buttonClick = document.querySelectorAll('.btn');
-// const imageBox = document.querySelector('.image-box');
 const bodyTheme = document.body;
 let sunImageSrc = "./images/sun ball.jpg";
 let moonImageSrc = "./images/moon.jpg";
 const imageBox = document.querySelector('.image-box');
-
-
+const animateSection = document.querySelector('.cloud__section-bg');
 let frontImage = true;
 function changeImage(){
   
@@ -14,7 +12,6 @@ function changeImage(){
   } else {
     imageBox.src = sunImageSrc;
   }
-
   frontImage = !frontImage;
 }
 
@@ -33,12 +30,49 @@ function animate() {
 
     requestAnimationFrame(step);
 }
+function animateNextImage() {
+  const duration = 1000;
+  const startTimestamp = performance.now();
+
+  function step(timestamp) {
+      const progress = Math.max(1 - ((timestamp - startTimestamp) / duration), 0); // Обчислення прогресу зникнення
+
+      imageBox.style.opacity = progress; // Застосування прогресу зникнення до прозорості зображення
+
+      if (progress > 0) { // Перевірка, чи анімація ще не завершилася
+          requestAnimationFrame(step); // Виклик наступного кроку анімації
+      }
+  }
+
+  requestAnimationFrame(step); // Початок анімації
+}
+
 
 buttonClick.forEach((button) => {
   button.addEventListener('click', () => {
-      animate();
-      changeImage();
-      bodyTheme.classList.toggle('dark');
+    animateNextImage();
+    setTimeout(changeImage, 2000);
+    setTimeout(animate, 2000);
+      
   });
 });
 
+let observer = new IntersectionObserver((entries, observer) => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      const targetElement = entry.target;
+      const statValues = targetElement.querySelectorAll('.cloud__section-bg');
+      
+      statValues.forEach(value => {
+        value.style.animationName = 'animateBg';
+      });
+    }
+  })
+}, { threshold: 0.3 });
+
+let sections = document.querySelectorAll('.cloud__section');
+if (sections.length) {
+  sections.forEach(section => {
+    observer.observe(section);
+  })
+}
